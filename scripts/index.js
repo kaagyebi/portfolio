fa7c28c95d03f5ab5c9101bf119016d050f57127
kaagyebi <kaagyebi@gmail.com>
2025-05-20 02:52:59 -0700

    added color to the hero

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
+// Declare variable to store navbar navbar element
+const navElement =  document.getElementById('navbar');
 // detect scroll event on document
 document.addEventListener('scroll', function() {
     // console.log(window.scrollY);
     if(window.scrollY > 70) {
         // change navbar bg to blue
-        document.getElementById('navbar').style.backgroundColor = '#0779E4'
+        navelement.style.backgroundColor = '#0779E4'
     } else{
         // change navbar bg transparent
-        document.getElementById('navbar').style.backgroundColor = 'transparent'
+        navelement.style.backgroundColor = 'transparent'
     }
     
 });
